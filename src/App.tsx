a1ed1d62b57e4df3591305a80adfd9054af11971
jc2100ph <jonathancruz2100ph@gmail.com>
2023-06-12 13:02:42 +0800

    feat: add fourth, fifth and last page of web portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import FirstPageComponent from "./components/first_page/FirstPageComponent"
 import SecondPageComponent from "./components/second_page/SecondPageComponent"
 import ThirdPageComponent from "./components/third_page/ThirdPageComponent"
 import FourthPageComponent from "./components/fourth_page/FourthPageComponent"
+import FifthPageComponent from "./components/fifth_page/FifthPageComponent"
+import LastPageComponent from "./components/last_page/LastPageComponent"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <SecondPageComponent />
         <ThirdPageComponent />
         <FourthPageComponent />
+        <FifthPageComponent />
+        <LastPageComponent />
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,15 @@ export default {
     extend: {
       backgroundImage: {
         'first_page_background': "url('./src/picture/first_page/first_page_background_enhance_webp.webp')",
-        'second_page_background': "url('./src/picture/second_page/second_page_background-enhance_webp.webp')"
+        'second_page_background': "url('./src/picture/second_page/second_page_background-enhance_webp.webp')",
+        'fifth_page_background': "linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('./src/picture/fifth_page/fifth_page_background-enhance_webp.webp')",
+        'last_page_background': "url('./src/picture/last_page/last_page_background.webp')",
       },
       fontFamily: {
         'roboto-condensed': ['Roboto Condensed', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+        'neutraFaceBold': ['Neutraface-Bold', 'sans-serif'],
+        'neutraFaceBook': ['Neutraface-Book', 'sans-serif']
       },
     },
   },
